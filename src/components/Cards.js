487bb0c9e44fab7794a41d320ab7d4f7c23d1c4e
Zach Frena <z.frena@gmail.com>
2022-01-06 19:21:25 -0700

    fixed photo links

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,14 +10,14 @@ function Cards () {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='/images/garmin-project.jpg'
+              src='images/garmin-project.JPG'
               text='Step Count Classification'
               label={['Python','Machine Learning','Scikit-Learn']}
               description="Investigating how machine learning algorithms can predict health and fitness outcomes when provided daily activity data."
               path='https://github.com/zachfrena/StepCountClassifier'
             />
             <CardItem
-              src='/images/vet-project.jpg'
+              src='/images/vet-project.JPG'
               text='Veterinary System Application'
               label={['Java','Full-Stack','React']}
               description = 'Full-stack application to manage, update, add, and delete animals and their associated health conditions. Created for the school of Veterinary Medicine at the University of Calgary.'
@@ -26,14 +26,14 @@ function Cards () {
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='/images/movie-theatre.jpg'
+              src='/images/movie-theatre.JPG'
               text='Movie Theatre Booking App' 
               label={['Java','Swing','MySQL']}
               description='Application that contains full functionality to book a movie ticket at a theatre.'
               path='https://github.com/zachfrena/MovieTheatreTicketRegistrationApp'
             />
             <CardItem
-              src='/images/personal-website.jpg'
+              src='/images/personal-website.JPG'
               text='Personal Website (this one!)'
               description='Portfolio website built using HTML5 and CSS3 in a React framework to add interactivity and state management.'
               label={['React','HTML','CSS']}
